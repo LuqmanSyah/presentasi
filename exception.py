591388e53c92889a6c32f7e943b2ae588e079081
Dapa102 <dafarafinurwansyah@gmail.com>
2024-11-25 22:11:13 +0700

    tambahan type error

--- exception.py
+++ exception.py
@@ -1,18 +1,28 @@
-def pembagian (a, b):
-    if b==0:
-        raise Exception("Angka pembagi tidak boleh nol.")
-    return a/b
-
 try:
-    hasil=pembagian (10, 2)
+    x = 10/2
+    hasil = x
     print (hasil)
     
 except Exception as e:
-    print (f"Kesalahan : {e}")
+    print ("Kesalahan : Tidak boleh dibagi 0")
 
 else:
     print ("Kode berjalan")
 
 finally:
     print ("Kode selesai dijalankan")   
+
+
+#Contoh Type error
+try:
+    hasil = "5" + 5
+    print(hasil)
     
+except TypeError as e:
+    print("Terjadi kesalahan: Tidak boleh memasukkan huruf!")
+
+else:
+    print("Berhasil")
+
+finally:
+    print("Kode berjalan")    
