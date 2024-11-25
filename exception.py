c80d48d01d6a91dc4785e1ab0f790d18e0b8fede
Dapa102 <dafarafinurwansyah@gmail.com>
2024-11-25 21:19:43 +0700

    Kaya gini bukan man?

--- exception.py
+++ exception.py
@@ -1,1 +1,18 @@
-print('hello')
+def pembagian (a, b):
+    if b==0:
+        raise Exception("Angka pembagi tidak boleh nol.")
+    return a/b
+
+try:
+    hasil=pembagian (10, 2)
+    print (hasil)
+    
+except Exception as e:
+    print (f"Kesalahan : {e}")
+
+else:
+    print ("Kode berjalan")
+
+finally:
+    print ("Kode selesai dijalankan")   
+    
